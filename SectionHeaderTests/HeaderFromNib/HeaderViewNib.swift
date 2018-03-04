93f0ce172b54d5e549ac9264fc6b9916185daf7a
Ken PIercece <kpierce8@gmail.com>
2018-03-04 14:41:29 -0800

    initial commit, simple header view from nib working

---
+++ SectionHeaderTests/HeaderFromNib/HeaderViewNib.swift
@@ -0,0 +1,26 @@
+//
+//  HeaderViewNib.swift
+//  SectionHeaderTests
+//
+//  Created by UberDominator on 3/4/18.
+//  Copyright © 2018 Frantic Goose Applications. All rights reserved.
+//
+
+import UIKit
+
+class HeaderViewNib: UITableViewHeaderFooterView {
+
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBAction func toggleHeaderBtn(_ sender: UIButton) {
+    }
+   
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- SectionHeaderTests/ViewController.swift
+++ SectionHeaderTests/ViewController.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let headerNib = UINib.init(nibName: "HeaderViewNib", bundle: Bundle.main)
+        tableView.register(headerNib, forHeaderFooterViewReuseIdentifier: "HeaderViewNib")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    var sectionHeaders = ["kings landing","captured","pentos"]
+    var sectionText = ["not a fun place", "there be bars of iron", "there be bars and wine"]
+    
+    var sectionData : [String : [String]] = ["kings landing": ["cersei", "joffrey", "tywin"],
+                                             "captured" : ["jaime", "tyrion"],
+                                             "pentos": ["tyrion"]]
+    
+    
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionHeaders.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (sectionData[sectionHeaders[section]]?.count)!
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let data = sectionData[sectionHeaders[indexPath.section]]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data?[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let aSection = tableView.dequeueReusableHeaderFooterView(withIdentifier: "HeaderViewNib") as! HeaderViewNib
+        aSection.headerLabel.text = sectionHeaders[section]
+        return aSection
+    }
+}
